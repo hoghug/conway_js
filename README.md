3bb75f6cb8b7c4f4ca568b49038b5c8bf5a3916b
chris lind <clind714@gmail.com>
2014-05-06 14:12:55 -0700

    converted to JS object

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 conway_js
 =========
-Conway's Game of Life using jQuery
-User can define the size of the grid
-User can select the cells to ostart live for the simulation.
+# Conway's Game of Life using jQuery
+
+# User can define the size of the grid
+
+# User can select the cells to ostart live for the simulation.
--- js/ui-scripts.js
+++ js/ui-scripts.js
@@ -1,128 +1,136 @@
 $(document).ready(function () {
-  spaces = [];
-  prevGenLive = [];
-  nextGenLive = [];
-  loopCounter = 0;
-  interval = '';
 
-  $('#build-grid').on('click', function() {
-    gridSize = $('#grid-size-input').val();
-    buildGrid(gridSize);
-    $('#grid-builder').remove();
-  });
+  conwayGame = {
 
-  function buildGrid(gridSize) {
-    $('#start-game').show();
-    for(x = 0; x < Math.pow(gridSize, 2); x++) {
-      spaces.push(x);
-      $('#conway-grid').append('<div data-cell="' + x + '" id="cell-' + x + '" class="conway-cell"></div>');
-      if(x % gridSize == gridSize - 1) {
-        $('#conway-grid').append('<div class="clear"></div>');
-      }
-    }
-    $('#conway-grid').css('width', gridSize * 17);
-    $('#conway-grid').after('<input id="hidden-gride-size" type="hidden" value="' + gridSize + '">');
+    spaces: [],
+    prevGenLive: [],
+    nextGenLive: [],
+    gridSize: 0,
+    loopCounter: 0,
+    interval: '',
 
-    startLiveCells();
-  }
+    setGrid: function() {
+      this.gridSize = $('#grid-size-input').val();
+      this.buildGrid();
+      $('#grid-builder').remove();
+    },
 
-  function startLiveCells() {
-    $('.conway-cell').on('click', function() {
-      $(this).toggleClass('live-cell');
-    });
-
-    $('#conway-grid').on('mousedown',function() {
-      $('.conway-cell').bind('mousedown mouseover', function(){
-        if(!($(this).hasClass('live-cell'))) {
-          $(this).addClass('live-cell');
+    buildGrid: function() {
+      $('#start-game').show();
+      for(x = 0; x < Math.pow(this.gridSize, 2); x++) {
+        this.spaces.push(x);
+        $('#conway-grid').append('<div data-cell="' + x + '" id="cell-' + x + '" class="conway-cell"></div>');
+        if(x % this.gridSize == this.gridSize - 1) {
+          $('#conway-grid').append('<div class="clear"></div>');
         }
+      }
+      $('#conway-grid').css('width', this.gridSize * 17);
+      this.setLiveCells();
+    },
+
+    setLiveCells: function() {
+      $('.conway-cell').on('click', function() {
+        $(this).toggleClass('live-cell');
       });
-    }).on('mouseup', function() {
-      $('.conway-cell').unbind('mousedown mouseover');
-    });
-  }
 
-  $('#start-game').on('click', function() {
-    $('#reset-game').show();
-    gridLoop();
-  });
+      $('#conway-grid').on('mousedown',function() {
+        $('.conway-cell').bind('mousedown mouseover', function(){
+          if(!($(this).hasClass('live-cell'))) {
+            $(this).addClass('live-cell');
+          }
+        });
+      }).on('mouseup', function() {
+        $('.conway-cell').unbind('mousedown mouseover');
+      });
+    },
 
-  function gridLoop() {
-    $('.conway-cell').each(function() {
-      checkNeighbors($(this));
-    });
+    startGame: function() {
+      $('#reset-game').show();
+      this.nextGenLive = [];
+      this.gridLoop();
+    },
 
-    interval = setInterval(function(){
-      nextGenLive = [];
+    gridLoop: function() {
       $('.conway-cell').each(function() {
-        checkNeighbors($(this));
+        conwayGame.checkNeighbors($(this));
       });
-      loopCounter += 1;
-    }, 100);
-  }
 
-  function checkNeighbors(current) {
-    gridSize = parseInt($('#hidden-gride-size').val());
-    curCell = current.data('cell');
-    rows = [gridSize * -1, 0, gridSize];
-    cols = [-1, 0, 1];
+      this.interval = setInterval(function(){
+        conwayGame.nextGenLive = [];
+        $('.conway-cell').each(function() {
+          conwayGame.checkNeighbors($(this));
+        });
+        conwayGame.loopCounter += 1;
+      }, 100);
+    },
 
-    if(curCell % gridSize == 0) { //left edge
-      cols.splice(cols.indexOf(-1), 1);
-    } else if(curCell % gridSize ==  gridSize - 1) { //right edge
-      cols.splice(cols.indexOf(1), 1);
-    }
-
-    liveNeighbors = [];
-
-    rows.forEach(function(r) {
-      cols.forEach(function(c) {
-        if(($('#cell-' + spaces[curCell + (r + c)]).hasClass('live-cell')) && ((curCell + r + c) != curCell)) {
-          liveNeighbors.push(curCell + (r + c));
-        }
+    checkNeighbors: function(current) {
+      grSz = this.gridSize * 1;
+      curCell = current.data('cell');
+      rows = [grSz * -1, 0, grSz];
+      cols = [-1, 0, 1];
+      if(curCell % grSz == 0) { cols.splice(cols.indexOf(-1), 1); } // left edge
+      if(curCell % grSz == grSz - 1) { cols.splice(cols.indexOf(1), 1); } // right edge
+      if(curCell - grSz < 0) { rows.splice(rows.indexOf(grSz * -1), 1); } // top edge
+      if(curCell + grSz >= this.spaces.length) { rows.splice(rows.indexOf(grSz * 1), 1); } // bottom edge
+      liveNeighbors = [];
+      rows.forEach(function(r) {
+        cols.forEach(function(c) {
+          if(($('#cell-' + conwayGame.spaces[curCell + (r + c)]).hasClass('live-cell')) && ((curCell + r + c) != curCell)) {
+            liveNeighbors.push(curCell + (r + c));
+          }
+        });
       });
-    });
-    buildNextGen(curCell, liveNeighbors);
-  }
+      this.buildNextGen(curCell, liveNeighbors);
+    },
 
-  function buildNextGen(curCell, liveNeighbors) {
-
-    if($('#cell-' + curCell).hasClass('live-cell')) {
-      if(liveNeighbors.length == 2 || liveNeighbors.length == 3) {
-        nextGenLive.push(curCell);
+    buildNextGen: function(curCell, liveNeighbors) {
+      if($('#cell-' + curCell).hasClass('live-cell')) {
+        if(liveNeighbors.length == 2 || liveNeighbors.length == 3) { this.nextGenLive.push(curCell); }
+      } else {
+        if(liveNeighbors.length == 3) { this.nextGenLive.push(curCell); }
       }
-    } else {
-      if(liveNeighbors.length == 3) {
-        nextGenLive.push(curCell);
-      }
-    }
+      if(curCell == this.spaces.length - 1) { this.lastCellGenerationLoop(); }
+    },
 
-    if(curCell == spaces.length - 1) { // when you reached the last cell, end of a generation loop
-      prevGenLive = [];
+    lastCellGenerationLoop: function() {
+      this.prevGenLive = [];
       $('.prev-live-cell').each(function(){
         $(this).removeClass('prev-live-cell');
       });
 
       $('.live-cell').each(function(){
-        prevGenLive.push($(this).data('cell'));
+        conwayGame.prevGenLive.push($(this).data('cell'));
         $(this).addClass('prev-live-cell');
         $(this).removeClass('live-cell');
       });
-      nextGenLive.forEach(function(curCell) {
+      this.nextGenLive.forEach(function(curCell) {
         $('#cell-' + curCell).addClass('live-cell');
       });
+    },
+
+    resetGame: function() {
+      clearInterval(this.interval);
+      this.prevGenLive = [];
+      this.nextGenLive = [];
+      $('.conway-cell').each(function(){
+        $(this).removeClass('live-cell');
+        $(this).removeClass('prev-live-cell');
+      });
+      this.setLiveCells();
     }
   }
 
+  $('#build-grid').on('click', function() {
+    conwayGame.setGrid();
+  });
+
+  $('#start-game').on('click', function() {
+    conwayGame.startGame();
+  })
+
   $('#reset-game').on('click', function() {
-    clearInterval(interval);
-    prevGenLive = [];
-    nextGenLive = [];
-    $('.conway-cell').each(function(){
-      $(this).removeClass('live-cell');
-      $(this).removeClass('prev-live-cell');
-    });
-    startLiveCells();
+    conwayGame.resetGame();
   })
 
 });
